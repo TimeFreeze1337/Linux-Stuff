27ca3322eed34e7cc79cca135940be7afc857842
TimeFreeze1337 <59339523+TimeFreeze1337@users.noreply.github.com>
2022-08-22 19:50:43 +0200

    Add files via upload

---
+++ update2.sh
@@ -0,0 +1,1 @@
+sudo pacman -S flatpak && flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install flathub org.yuzu_emu.yuzu && flatpak install flathub net.davidotek.pupgui2 && flatpak install flathub net.pcsx2.PCSX2 && flatpak install flathub com.usebottles.bottles
